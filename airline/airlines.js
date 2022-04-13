6b1e7be89286646a5e0e918295a27138328174ec
Marian <marianlopig@gmail.com>
2022-04-13 19:57:02 +0200

    errors with slint changed

--- airline/airlines.js
+++ airline/airlines.js
@@ -1,54 +1,54 @@
-let flights = [
+const flights = [
 
-  { id: 00, to: 'Bilbao', from: 'Barcelona', cost: 1600, scale: false },
+  { id: 0, to: 'Bilbao', from: 'Barcelona', cost: 1600, scale: false },
 
-  { id: 01, to: 'New York', from: 'Barcelona', cost: 700, scale: false },
+  { id: 1, to: 'New York', from: 'Barcelona', cost: 700, scale: false },
 
-  { id: 02, to: 'Los Angeles', from: 'Madrid', cost: 1100, scale: true },
+  { id: 2, to: 'Los Angeles', from: 'Madrid', cost: 1100, scale: true },
 
-  { id: 03, to: 'Paris', from: 'Barcelona', cost: 210, scale: false },
+  { id: 3, to: 'Paris', from: 'Barcelona', cost: 210, scale: false },
 
-  { id: 04, to: 'Roma', from: 'Barcelona', cost: 150, scale: false },
+  { id: 4, to: 'Roma', from: 'Barcelona', cost: 150, scale: false },
 
-  { id: 05, to: 'London', from: 'Madrid', cost: 200, scale: false },
+  { id: 5, to: 'London', from: 'Madrid', cost: 200, scale: false },
 
-  { id: 06, to: 'Madrid', from: 'Barcelona', cost: 90, scale: false },
+  { id: 6, to: 'Madrid', from: 'Barcelona', cost: 90, scale: false },
 
-  { id: 07, to: 'Tokyo', from: 'Madrid', cost: 1500, scale: true },
+  { id: 7, to: 'Tokyo', from: 'Madrid', cost: 1500, scale: true },
 
-  { id: 08, to: 'Shangai', from: 'Barcelona', cost: 800, scale: true },
+  { id: 8, to: 'Shangai', from: 'Barcelona', cost: 800, scale: true },
 
-  { id: 09, to: 'Sydney', from: 'Barcelona', cost: 150, scale: true },
+  { id: 9, to: 'Sydney', from: 'Barcelona', cost: 150, scale: true },
 
   { id: 10, to: 'Tel-Aviv', from: 'Madrid', cost: 150, scale: false }
 ];
-
-function getFlightInfo(flight) {
-  return "El vuelo con origen: " + flight.from + " y destino: " + flight.to + " tiene un coste de " + flight.cost + "€ " + checkStopOver(flight);
-};
 
 function checkStopOver(flight) {
   if (flight.scale) {
     return "y tiene una escala.";
   }
-  else {
-    return "y no realiza ninguna escala";
-  }
+  return "y no realiza ninguna escala";
 }
+
+function getFlightInfo(flight) {
+  return `El vuelo con origen: ${flight.from} y destino: ${flight.to} tiene un coste de ${flight.cost} € ${checkStopOver(flight)}`;
+};
+
 function printFlightsInfo() {
   let flightsInfo = "";
   flights.forEach(flight => {
-    flightsInfo += getFlightInfo(flight) + "\n";
+    flightsInfo += `${getFlightInfo(flight)}\n`;
   })
-  alert(flightsInfo);
+  return flightsInfo
 }
+
 function printFlightCostAverage() {
   let flightsCost = 0;
   flights.forEach(flight => {
     flightsCost += flight.cost
   })
-  let flightAverage = flightsCost / flights.length;
-  alert("El precio medio es " + flightAverage.toFixed(2) + "€");
+  const flightAverage = (flightsCost / flights.length).toFixed(2);
+  return flightAverage;
 }
 function printFlightsWithStopOver() {
   let flightsStopOver = 0;
@@ -57,15 +57,14 @@ function printFlightsWithStopOver() {
       flightsStopOver++
     }
   })
-  alert("Hay " + flightsStopOver + " vuelos con escala");
+  return (`Hay ${flightsStopOver} vuelos con escala`);
 }
 function printLastFiveDestinations() {
-  let index = flights.length - 1
-  alert("Los ultimos destinos son: " + flights[index].to + ", " + flights[index - 1].to + ", " + flights[index - 2].to + ", " + flights[index - 3].to + " y " + flights[index - 4].to);
+  const index = flights.length - 1
+  return (`Los ultimos destinos son: ${flights[index].to}, ${flights[index - 1].to}, ${flights[index - 2].to}, ${flights[index - 3].to} y ${flights[index - 4].to}`);
 }
 
-let personName = prompt("Introduce tu nombre, por favor");
-alert("Hola " + personName + ", Bienvenido a nuestro Airline");
+
 printFlightsInfo();
 printFlightCostAverage();
 printFlightsWithStopOver();
